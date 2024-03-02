ce31fba1aeb9382fe0d8a0078144fb741698acff
Chirag Khanna <ckhanna@horizontal.com>
2024-03-02 18:05:46 +0530

    modal changes for scheduled notification

---
+++ src/Feature/DowntimeNotification/Constants.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.Data;
+
+namespace DowntimeNotification
+{
+    public static class Constants
+    {
+        public static readonly ID DownNotificationSettingsId = new ID("{3AE921A8-FAA1-4C83-B883-727864931727}");
+    }
+    public static class Templates
+    {
+        public static readonly ID DowntimeNotificationItemTemplateId = new ID("{29786CA7-8DBE-41DE-880D-77B649C0F98B}");
+    }
+}
